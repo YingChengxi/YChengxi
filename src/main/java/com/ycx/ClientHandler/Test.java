5379fd11c264007d4099b005beb96c5464d10156
Rong0 <1142847363@qq.com>
2023-10-07 18:47:31 +0800

    1111

--- src/main/java/com/ycx/ClientHandler/Test.java
+++ src/main/java/com/ycx/ClientHandler/Test.java
@@ -6,16 +6,11 @@ import fi.dy.masa.malilib.hotkeys.IKeybind;
 import fi.dy.masa.malilib.hotkeys.KeyAction;
 
 
-
-
-
 public class Test implements IHotkeyCallback {
 
     @Override
     public boolean onKeyAction(KeyAction action, IKeybind key) {
 
-
-        System.out.print(action.toString() + key);
 
        return true;
     }
